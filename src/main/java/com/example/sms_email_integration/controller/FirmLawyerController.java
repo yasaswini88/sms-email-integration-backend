98cbcaab3b1969d0e1422951741a52af2f2dab83
EC2 Default User <ec2-user@ip-172-31-94-87.ec2.internal>
2025-02-12 07:17:08 +0000

    Added before adding csv file

--- src/main/java/com/example/sms_email_integration/controller/FirmLawyerController.java
+++ src/main/java/com/example/sms_email_integration/controller/FirmLawyerController.java
@@ -1,8 +1,12 @@
 package com.example.sms_email_integration.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sms_email_integration.entity.Customer;
+import com.example.sms_email_integration.entity.FirmClientMapping;
 import com.example.sms_email_integration.entity.FirmLawyer;
 import com.example.sms_email_integration.repository.CustomerRepository;
+import com.example.sms_email_integration.repository.FirmClientMappingRepository;
 import com.example.sms_email_integration.repository.FirmLawyerRepository;
 
 @RestController
@@ -22,6 +28,9 @@ public class FirmLawyerController {
 
     private final FirmLawyerRepository firmLawyerRepository;
     private final CustomerRepository customerRepository;
+
+    @Autowired
+    private FirmClientMappingRepository firmClientMappingRepository;
 
     // Constructor injection for both repositories:
     public FirmLawyerController(
@@ -53,28 +62,45 @@ public class FirmLawyerController {
         return ResponseEntity.ok(savedLawyer);
     }
 
-    //      @PostMapping("/bulk-insert")
-    // public ResponseEntity<?> bulkInsertLawyers(@RequestBody List<FirmLawyerDto> lawyerDtos) {
-    //     try {
-    //         List<FirmLawyer> lawyers = lawyerDtos.stream().map(dto -> {
-    //             FirmLawyer lawyer = new FirmLawyer();
-    //             lawyer.setLawyerName(dto.getLawyerName());
-    //             lawyer.setLawyerMail(dto.getLawyerMail());
-    //             lawyer.setLawyerPassword(dto.getLawyerPassword());
-    //             lawyer.setLawyerRole(dto.getLawyerRole());
+         @PostMapping("/bulk-insert")
+    public ResponseEntity<?> bulkInsertLawyers(@RequestBody List<HashMap<String,String>> lawyerDtos,
+                            @Param("custiId") Long custiId) {   
+        try {
+            System.out.println("lawyerDtos: " + lawyerDtos);
+            System.err.println("custiId: " + custiId);
+            for (HashMap<String, String> lawyerDto : lawyerDtos) {
 
-    //             // Find the firm (Customer) and set it
-    //             Customer firm = customerRepository.findById(dto.getFirm().getCusti_id())
-    //                     .orElseThrow(() -> new RuntimeException("Firm not found: " + dto.getFirm().getCusti_id()));
-    //             lawyer.setFirm(firm);
-    //             return lawyer;
-    //         }).collect(Collectors.toList());
+                // add a duplicate check here
+                
+                FirmLawyer newLawyer = new FirmLawyer();
+                newLawyer.setLawyerName("");
+                newLawyer.setLawyerMail(lawyerDto.get("Lawyer Email"));
+                newLawyer.setLawyerPassword("1234");
+                newLawyer.setLawyerRole("LAWYER");
 
-    //         firmLawyerRepository.saveAll(lawyers);  // Bulk insert operation
-    //         return ResponseEntity.ok("Bulk insert successful");
-    //     } catch (Exception e) {
-    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error inserting data: " + e.getMessage());
-    //     }
-    // }
+
+                // 1) Find the firm
+                Optional<Customer> optionalCustomer = customerRepository.findById(custiId);
+                if (optionalCustomer.isEmpty()) {
+                    return ResponseEntity.notFound().build();
+                }
+                // 2) Assign
+                newLawyer.setFirm(optionalCustomer.get());
+                // 3) Save
+                firmLawyerRepository.save(newLawyer);
+
+                FirmClientMapping firmClientMapping = new FirmClientMapping();
+                firmClientMapping.setFirm(optionalCustomer.get());
+                firmClientMapping.setFirmLawyer(newLawyer);
+                firmClientMapping.setCaseType("Unknown");
+                firmClientMapping.setClientPhoneNumber(lawyerDto.get("Client Phone Numnber"));
+                firmClientMappingRepository.save(firmClientMapping);
+            }
+            return ResponseEntity.ok("Data inserted successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error inserting data: " + e.getMessage());
+        }
+    }
 
 }
